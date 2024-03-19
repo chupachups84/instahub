c8b30a04bb9dc0b49864880ac257580a02ea7589
pavel-chernyshev <pasha7w@gmail.com>
2024-03-19 22:46:48 +0300

    solve #1 issue

--- backend/instahub-app/src/main/java/vistar/practice/demo/InstaHubApplication.java
+++ backend/instahub-app/src/main/java/vistar/practice/demo/InstaHubApplication.java
@@ -1,58 +1,12 @@
 package vistar.practice.demo;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
-import liquibase.Contexts;
-import liquibase.LabelExpression;
-import liquibase.Liquibase;
-import liquibase.database.Database;
-import liquibase.database.DatabaseFactory;
-import liquibase.database.jvm.JdbcConnection;
-import liquibase.exception.LiquibaseException;
-import liquibase.resource.ClassLoaderResourceAccessor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
+@SpringBootApplication
 public class InstaHubApplication {
 
-    private static void databaseUpdate() throws SQLException, LiquibaseException {
-        try (
-                Connection connection = DriverManager.getConnection(
-                        "jdbc:postgresql://localhost:5432/postgres",
-                        "postgres",
-                        "postgres"
-                )
-        ) {
-            Database database = DatabaseFactory.getInstance()
-                    .findCorrectDatabaseImplementation(new JdbcConnection(connection));
-            try (
-                    Liquibase liquibase = new liquibase.Liquibase(
-                            "config/liquibase/master.xml",
-                            new ClassLoaderResourceAccessor(),
-                            database
-                    )
-            ) {
-                liquibase.update(new Contexts(), new LabelExpression());
-            }
-        } catch (SQLException | LiquibaseException ex) {
-            throw ex;
-        }
-    }
-
     public static void main(String[] args) {
-
-        try {
-            databaseUpdate();
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            return;
-        }
-
-        final EntityManagerFactory entityManagerFactory =
-                Persistence.createEntityManagerFactory("vistar.practice.demo.unit");
-        final EntityManager entityManager = entityManagerFactory.createEntityManager();
+        SpringApplication.run(InstaHubApplication.class, args);
     }
 }
