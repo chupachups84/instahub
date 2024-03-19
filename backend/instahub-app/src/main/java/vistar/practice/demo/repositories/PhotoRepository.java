817388f0ee8fe365b4c990a6cbc170e58118546d
pavel-chernyshev <pasha7w@gmail.com>
2024-03-19 23:52:46 +0300

    add entities and repositories for comment, hashtag, photo

---
+++ backend/instahub-app/src/main/java/vistar/practice/demo/repositories/PhotoRepository.java
@@ -0,0 +1,10 @@
+package vistar.practice.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import vistar.practice.demo.models.PhotoEntity;
+
+@Repository
+public interface PhotoRepository extends JpaRepository<PhotoEntity,Long> {
+
+}
