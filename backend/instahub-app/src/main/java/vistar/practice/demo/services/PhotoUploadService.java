2bd8cd782a61b29dd26ca9952a8cfc02093ded0d
pavel-chernyshev <pasha7w@gmail.com>
2024-04-07 04:07:42 +0300

    remove unused imports

--- backend/instahub-app/src/main/java/vistar/practice/demo/services/PhotoUploadService.java
+++ backend/instahub-app/src/main/java/vistar/practice/demo/services/PhotoUploadService.java
@@ -3,16 +3,11 @@ package vistar.practice.demo.services;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.RestTemplate;
 import vistar.practice.demo.dtos.photo.PhotoUploadDto;
 import vistar.practice.demo.kafka.KafkaSender;
 import vistar.practice.demo.mappers.PhotoUploadMapper;
-import vistar.practice.demo.repositories.UserRepository;
-
-import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
@@ -23,9 +18,7 @@ public class PhotoUploadService {
     @Value("${kafka.topic.photo}")
     private String photoTopic;
     private final PhotoUploadMapper photoUploadMapper;
-
     private final KafkaSender kafkaSender;
-    private final UserRepository userRepository;
 
     public void store(PhotoUploadDto photoUploadDto) {
 
