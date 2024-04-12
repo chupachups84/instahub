bf13a2d22cf0b29a255073a21285b80091c6ee3f
k4r3l1ns <makcum.kyp4ehkob@gmail.com>
2024-04-12 21:35:51 +0300

    #30 Integrated Specification API into project

---
+++ backend/instahub-app/src/main/java/vistar/practice/demo/configs/specification/Condition.java
@@ -0,0 +1,58 @@
+package vistar.practice.demo.configs.specification;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Builder
+@Getter
+@Setter
+public class Condition {
+
+    private final String fieldName;
+
+    private final Comparable value;
+    private final List<Comparable> values;
+
+    private final OperationType operation;
+
+    private LogicalOperatorType logicalOperator;
+
+    @Getter
+    public enum OperationType {
+
+        GREATER(">"),
+        LESS("<"),
+        EQUALS("="),
+        IN ("in"),
+        BEGINS_WITH("begins with");
+
+        private final String name;
+
+        private final static Map<String, OperationType> _map;
+
+        static {
+
+            _map = Stream.of(values()).collect(Collectors.toMap(OperationType::getName, Function.identity()));
+        }
+
+        OperationType(String name) {
+            this.name = name;
+        }
+
+        public static OperationType resolveByName(String name) {
+            return _map.getOrDefault(name, null);
+        }
+    }
+
+    public enum LogicalOperatorType {
+
+        AND, OR, END
+    }
+}
