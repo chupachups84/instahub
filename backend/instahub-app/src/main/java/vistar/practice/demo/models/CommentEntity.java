2b1dbaa1440756f1d2eead3b5440e4c84f240f9f
k4r3l1ns <makcum.kyp4ehkob@gmail.com>
2024-04-12 21:40:26 +0300

    #30 Mapping changes & new method

--- backend/instahub-app/src/main/java/vistar/practice/demo/models/CommentEntity.java
+++ backend/instahub-app/src/main/java/vistar/practice/demo/models/CommentEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import vistar.practice.demo.models.photo.PhotoEntity;
 
 import java.time.Instant;
 
