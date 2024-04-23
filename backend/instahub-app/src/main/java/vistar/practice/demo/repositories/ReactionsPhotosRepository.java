2095378413c7734060327da91bb264e24a548157
Яровой Данила <arovojdanila1@gmail.com>
2024-04-23 17:22:43 +0300

    minor fix

--- backend/instahub-app/src/main/java/vistar/practice/demo/repositories/ReactionsPhotosRepository.java
+++ backend/instahub-app/src/main/java/vistar/practice/demo/repositories/ReactionsPhotosRepository.java
@@ -12,5 +12,5 @@ import java.util.Optional;
 @Repository
 public interface ReactionsPhotosRepository extends JpaRepository<ReactionsPhotosEntity, Long> {
     List<ReactionsPhotosEntity> findAllByPhoto(PhotoEntity photo);
-    Optional<ReactionsPhotosEntity> findByReactionByAndPhoto(Long reactionBy, Long photo_id);
+    Optional<ReactionsPhotosEntity> findByReactionByAndPhoto(UserEntity reactionBy, PhotoEntity photo_id);
 }
