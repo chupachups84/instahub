b7fb812b69a2110c38a1a458baf869b3b6f2b7c2
k4r3l1ns <makcum.kyp4ehkob@gmail.com>
2024-04-26 17:00:04 +0300

    #45 Deleted odd code

--- frontend/src/components/sign-in/SignInForm.js
+++ frontend/src/components/sign-in/SignInForm.js
@@ -101,23 +101,3 @@ const SignInForm = () => {
 }
 
 export default SignInForm
-
-
-// import React, {Component} from "react";
-// import '../sign-in/SignInForm.css'
-//
-// class SignInForm extends Component {
-//     render() {
-//         return (
-//             <div className={"sign-in__form"}>
-//                 <div className={"create-account"}>Welcome back</div>
-//                 <div className={"greetings"}>Sign in to see your friends photos!</div>
-//                 <input type={"text"} className={"input-user"} placeholder={"Enter your username"}/>
-//                 <input type={"password"} className={"input-user"} placeholder={"Enter your password"}/>
-//                 <button className={"button"}>Sign In</button>
-//             </div>
-//         );
-//     }
-// }
-//
-// export default SignInForm;
