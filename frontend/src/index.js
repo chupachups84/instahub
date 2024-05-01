43b0d1ca467dd8fb3c7cfaa203dec02c0578a84c
pavel-chernyshev <pasha7w@gmail.com>
2024-05-01 12:40:18 +0300

    #53 init template

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import SignUpPage from "./pages/sign-up/SignUpPage";
 import reportWebVitals from './reportWebVitals';
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom";
+import {createBrowserRouter, RouterProvider,} from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import SignInPage from "./pages/sign-in/SignInPage";
+import MyProfilePage from "./pages/my-profile/MyProfilePage";
 import {Provider} from "react-redux";
 import store from "./store/instahub/components/authentication/store";
 
@@ -24,6 +22,10 @@ const router = createBrowserRouter([
     {
         path: "/sign-in",
         element: <SignInPage/>
+    },
+    {
+        path: "/:id",
+        element: <MyProfilePage/>
     }
 ]);
 
