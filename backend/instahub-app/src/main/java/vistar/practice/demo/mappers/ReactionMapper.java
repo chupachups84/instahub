dec4dc0b94460cc4134a37cdad9a147f092b6ad7
k4r3l1ns <makcum.kyp4ehkob@gmail.com>
2024-05-03 18:33:51 +0300

    #56 Integrated reactions and reposts into workflow

--- backend/instahub-app/src/main/java/vistar/practice/demo/mappers/ReactionMapper.java
+++ backend/instahub-app/src/main/java/vistar/practice/demo/mappers/ReactionMapper.java
@@ -6,14 +6,28 @@ import org.mapstruct.MappingConstants;
 import vistar.practice.demo.dtos.reaction.ReactionCreateEditDto;
 import vistar.practice.demo.dtos.reaction.ReactionReadDto;
 import vistar.practice.demo.models.ReactionEntity;
+import vistar.practice.demo.models.ReactionsPhotosEntity;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING, injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface ReactionMapper {
     ReactionReadDto toReadDto (ReactionEntity reactionEntity);
     ReactionEntity toEntity (ReactionCreateEditDto createEditDto);
-    default ReactionEntity copyToEntityFromDto(ReactionEntity entity ,ReactionCreateEditDto createEditDto) {
-        entity.setName(createEditDto.getName());
-        return entity;
+
+    default Map<String, Integer> toHashMap(List<ReactionsPhotosEntity> reactionsPhotosList) {
+        Map<String, Integer> hashMap = new HashMap<>();
+        for (var entity : reactionsPhotosList) {
+            var reactionName = entity.getReaction().getName().name();
+            if (!hashMap.containsKey(reactionName)) {
+                hashMap.put(reactionName, 1);
+            } else {
+                hashMap.put(reactionName, hashMap.get(reactionName) + 1);
+            }
+        }
+        return hashMap;
     }
 }
