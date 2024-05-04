df0a29315c391656965d6f6a040f54d79dcf9382
k4r3l1ns <makcum.kyp4ehkob@gmail.com>
2024-05-04 13:08:56 +0300

    #53 Fixed storing on reload, paging (todo: rendering)

--- frontend/src/components/my-profile/ProfileMain.js
+++ frontend/src/components/my-profile/ProfileMain.js
@@ -10,40 +10,39 @@ const ProfileMain = (username) => {
 
     const dispatch: Dispatch = useDispatch();
 
-    const [profilePhotos, setProfilePhotos] = useState([]);
-
-    const [page, setPage] = useState(0);
+    const [profilePhotos, setProfilePhotos] = useState(
+        JSON.parse(localStorage.getItem("profilePhotos")) === null ?
+            [] : JSON.parse(localStorage.getItem("profilePhotos"))
+    );
 
     let size = 3;
 
-    const fetchNextPage = () => {
-        setPage(page + 1);
-        dispatch(fetchPhotos(page, size, username.username));
-        setProfilePhotos(
-            JSON.parse(localStorage.getItem("profilePhotos")) === null ?
-            [] : JSON.parse(localStorage.getItem("profilePhotos"))
-        )
-        console.log("page: " + page)
-        console.log("prof photos: " + profilePhotos.length)
+    const [nextPage, setNextPage] = useState(
+        JSON.parse(localStorage.getItem("profilePhotos")) === null ?
+            1 : Math.floor((JSON.parse((localStorage.getItem("profilePhotos"))).length - 1) / size) + 1
+    );
+
+
+    const fetchNextPage = (page) => {
+        console.log("page = " + page)
+        dispatch(fetchPhotos(page, size, username.username))
+            .then(
+                setProfilePhotos(
+                    JSON.parse(localStorage.getItem("profilePhotos")) === null ?
+                        [] : JSON.parse(localStorage.getItem("profilePhotos"))
+                )
+            );
+        setNextPage(page + 1);
     }
 
 
     document.addEventListener(
         'DOMContentLoaded', function() {
-            if (JSON.parse(localStorage.getItem("profilePhotos")) === null) {
-                dispatch(fetchPhotos(0, size, username.username));
+            if (profilePhotos.length === 0) {
+                console.log("initial fetch")
+                fetchNextPage(0);
+                console.log("nextPage after initial fetch: " + nextPage)
             }
-            // else {
-            //     setProfilePhotos(
-            //         JSON.parse(localStorage.getItem("profilePhotos"))
-            //     );
-            // }
-            setProfilePhotos(
-                JSON.parse(localStorage.getItem("profilePhotos")) === null ?
-                    [] : JSON.parse(localStorage.getItem("profilePhotos"))
-            );
-            fetchNextPage();
-            console.log("first render: " + profilePhotos.length)
         }
     )
 
@@ -51,28 +50,37 @@ const ProfileMain = (username) => {
         <div>
             <div className="container">
                 <div className="gallery">
-                    {profilePhotos.map((photoBase64) => (
+                    {profilePhotos.map((photoBase64, index) => (
                         <div
+                            onClick={() =>
+                                console.log(index) //insert here axios FeedPhotoDto call (creationOffset equals index)
+                            }
                             className="gallery-item"
                             tabIndex="0"
                         >
                             <img src={`data:image/jpg;base64,${photoBase64}`}
-                                 className="gallery-image" alt=""/>
+                                 className="gallery-image"
+                                 alt="image"
+                            />
                             <div className="gallery-item-info">
-                                <ul>
-                                    <li className="gallery-item-likes"><span className="visually-hidden">Likes:</span><i
-                                        className="fas fa-heart" aria-hidden="true"></i> 56
-                                    </li>
-                                    <li className="gallery-item-comments"><span
-                                        className="visually-hidden">Comments:</span><i className="fas fa-comment"
-                                                                                       aria-hidden="true"></i> 2
-                                    </li>
-                                </ul>
+
+                            {/*   FOLLOWING CODE IS BROKEN BUT MIGHT BE USED IN FUTURE    */}
+
+                            {/*    <ul>*/}
+                            {/*        <li className="gallery-item-likes"><span className="visually-hidden">Likes:</span><i*/}
+                            {/*            className="fas fa-heart" aria-hidden="true"></i> 56*/}
+                            {/*        </li>*/}
+                            {/*        <li className="gallery-item-comments"><span*/}
+                            {/*            className="visually-hidden">Comments:</span><i className="fas fa-comment"*/}
+                            {/*                                                           aria-hidden="true"></i> 2*/}
+                            {/*        </li>*/}
+                            {/*    </ul>*/}
+
                             </div>
                         </div>
                     ))}
                 </div>
-                <button onClick={() => fetchNextPage()}>Load more</button>
+                <button onClick={() => fetchNextPage(nextPage)}>Load more</button>
             </div>
         </div>
     )
