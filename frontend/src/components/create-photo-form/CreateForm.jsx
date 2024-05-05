68482c6891b013817a3216cbb9d005d7cfcebb18
Яровой Данила <arovojdanila1@gmail.com>
2024-05-05 13:53:46 +0300

    #50 add isAvatar checkbox

--- frontend/src/components/create-photo-form/CreateForm.jsx
+++ frontend/src/components/create-photo-form/CreateForm.jsx
@@ -6,6 +6,7 @@ const CreateForm = ({ handleClose }) => {
   const [file, setFile] = useState(null);
   const [description, setDescription] = useState("");
   const [hashtags, setHashtags] = useState([]);
+  const [isAvatar, setIsAvatar] = useState(false);
 
   const handleFileChange = (event) => {
 
@@ -30,12 +31,17 @@ const CreateForm = ({ handleClose }) => {
     setDescription(inputString);
   };
 
+  const handleCheckBox = () => {
+    setIsAvatar(!isAvatar);
+  }
+
   async function handleSubmit(e) {
     e.preventDefault();
     const photoInfo = {
       file: file,
       description: description,
-      hashtags: hashtags
+      hashtags: hashtags,
+      isAvatar: isAvatar
     };
     const response = await axios.post(
         "localhost:8080/api/v1/photos",
@@ -58,6 +64,10 @@ const CreateForm = ({ handleClose }) => {
               {file && (
                   <img className={"img-preview"} src={file} alt="Uploaded" />
               )}
+              <div>
+                <label htmlFor="isAvatar">Обновить аватар </label>
+                <input type="checkbox" id="isAvatar" onClick={handleCheckBox}/>
+              </div>
             </div>
             <div>
               <div>
