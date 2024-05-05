61115fbb413b7d7193f785e1b79d8ce48188f361
k4r3l1ns <makcum.kyp4ehkob@gmail.com>
2024-05-05 20:35:41 +0300

    #62 Fixed username

--- frontend/src/store/instahub/components/users/services/userDataService.js
+++ frontend/src/store/instahub/components/users/services/userDataService.js
@@ -1,30 +1,37 @@
 import axios from "axios";
 import authHeader from "../../authentication/services/authHeader";
 
-
 class UserDataService {
 
     loadUserData(username) {
         return axios.all([
             axios.get("api/v1/users", {
                 params: {
-                    username: username.username
+                    username: username
                 },
                 withCredentials: true,
                 headers: authHeader().Authorization
             }),
-            axios.get("api/v1/users/" + username.username + "/followers/count", {
+            axios.get("api/v1/users/" + username + "/followers/count", {
                 withCredentials: true,
                 headers: authHeader().Authorization
             }),
-            axios.get( "api/v1/users/" + username.username + "/follows/count", {
+            axios.get( "api/v1/users/" + username + "/follows/count", {
                 withCredentials: true,
                 headers: authHeader().Authorization
+            }),
+            axios.get("api/v1/users/" + username + "/relation", {
+                params: {
+                    username: username
+                },
+                headers: authHeader().Authorization,
+                withCredentials: true
             })
-        ]).then(axios.spread((userDataResponse, followersCountResponse, followsCountResponse) => {
+        ]).then(axios.spread((userDataResponse, followersCountResponse, followsCountResponse, relationResponse) => {
             if (userDataResponse.data && followersCountResponse.data && followsCountResponse.data) {
                 userDataResponse.data.followersCount = followersCountResponse.data.count;
                 userDataResponse.data.followsCount = followsCountResponse.data.count;
+                userDataResponse.data.relation = relationResponse.data;
                 localStorage.setItem("userData", JSON.stringify(userDataResponse.data));
             }
             return userDataResponse.data;
