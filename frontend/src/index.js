30cc451fc30c532767dec787c5f732b3cf9d4451
pavel-chernyshev <pasha7w@gmail.com>
2024-05-06 12:25:20 +0300

    #65 move redux store in single file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,8 +8,9 @@ import HomePage from "./pages/home/HomePage";
 import SignInPage from "./pages/sign-in/SignInPage";
 import ProfilePage from "./pages/my-profile/ProfilePage";
 import {Provider} from "react-redux";
-import store from "./store/instahub/components/authentication/store";
+import store, {persistor} from "./store/instahub";
 import ActivateUserPage from "./pages/activate-user/ActivateUserPage";
+import {PersistGate} from "redux-persist/integration/react";
 
 const router = createBrowserRouter([
     {
@@ -37,11 +38,12 @@ const router = createBrowserRouter([
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
-        <React.StrictMode>
-            <RouterProvider router={router} />
-        </React.StrictMode>
+        <PersistGate loading={null} persistor={persistor}>
+            <React.StrictMode>
+                <RouterProvider router={router}/>
+            </React.StrictMode>
+        </PersistGate>
     </Provider>
-
 );
 
 reportWebVitals();
